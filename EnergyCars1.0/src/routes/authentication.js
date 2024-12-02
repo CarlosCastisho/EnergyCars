34ef312d8d13a3e877f6e0646d466aca3f9666cd
mylian21 <mpenam02@gmail.com>
2024-12-02 19:30:18 -0300

    Editar perfil user, botón cancelar y contacto

--- EnergyCars1.0/src/routes/authentication.js
+++ EnergyCars1.0/src/routes/authentication.js
@@ -180,9 +180,10 @@ router.get('/editarUser/:ID_USER', isLoggedIn, async (req,res) => {
 router.post('/editarUser/:ID_USER', isLoggedIn, async (req,res) => {
     const { ID_USER } = req.params;
     
-    const { user_correo, user_telefono, user_contrasenia } = req.body;
+    const { user_nombre, user_apellido, user_telefono, user_contrasenia } = req.body;
     const editarUser = {
-        user_correo,
+        user_nombre,
+        user_apellido,
         user_telefono,
         user_contrasenia: await helpers.encryptContrasenia(user_contrasenia)
     };
